9f8246be7ddf2d8bb7b822e1d8b9bf157a98915e
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-03-05 14:20:13 -0800

    Added rspec test

---
+++ spec/rack/accept_default_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Rack::AcceptDefault do
+  include Rack::Test::Methods
+
+  def app
+    rack_app = lambda { |env| [200, {"Content-Type" => "text/plain"}, [ env["HTTP_ACCEPT"] ]] }
+    Rack::AcceptDefault.new(rack_app)
+  end
+
+  it "should set */* as a default header" do
+    get '/'
+    last_response.should be_ok
+    last_response.body.should == '*/*'
+  end
+
+  it "should not override given Accept header" do
+    get '/', {}, { 'HTTP_ACCEPT' => "application/json" }
+    last_response.should be_ok
+    last_response.body.should == 'application/json'
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'rack-accept-default'
+require 'rack/test'
+require 'rspec'
+
+RSpec.configure do |config|
+end
